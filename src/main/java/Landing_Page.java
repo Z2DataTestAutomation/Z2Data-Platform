a5e27506f16971a8407d13f28ee9d503b7d47ca5
Z2Data Test Automation <muhammad.sultan@z2data.com>
2021-01-20 00:20:44 +0200

    Z2Data Platform

---
+++ src/main/java/Landing_Page.java
@@ -0,0 +1,19 @@
+import com.shaft.gui.element.ElementActions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+public class Landing_Page {
+    private WebDriver driver;
+
+    public Landing_Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
