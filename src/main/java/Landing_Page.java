80e17ede8112224d2223bc47f9acf930b136d140
BasmaBadr <basma.badr1010@gmail.com>
2021-01-24 16:43:56 +0200

    BG_24012021_16:45

--- src/main/java/Landing_Page.java
+++ src/main/java/Landing_Page.java
@@ -1,19 +1,14 @@
+
 import com.shaft.gui.element.ElementActions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
-
-public class Landing_Page {
-    private WebDriver driver;
-
+public class Landing_Page extends Page_Base{
     public Landing_Page(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
+
 }
+
+
