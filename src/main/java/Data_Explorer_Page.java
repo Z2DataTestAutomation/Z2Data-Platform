0426a949aace5c2add5582ac30655ce332394b86
BasmaBadr <basma.badr1010@gmail.com>
2021-01-27 16:12:21 +0200

    BG_27012021_16.12

--- src/main/java/Data_Explorer_Page.java
+++ src/main/java/Data_Explorer_Page.java
@@ -42,4 +42,17 @@ public class Data_Explorer_Page {
     public By Search_glass = By.cssSelector("div[class='input-group input-group-sm float-left width-300'] i[class='icon-Search-Query ic-search font-small-4 cursor-pointer']");
     public By More_filter_Btnn = By.cssSelector("div.input-group.input-group-sm.float-left.width-300:nth-child(1) > input");
     public By MPN_IPN_Switcher = By.cssSelector("div[class='jtoggler-control']");
+
+    // Data_Explorer_ِBOM View
+    //MPN and IPN View
+    public By ToggleButton = By.cssSelector("div[class='jtoggler-control']");
+    public By Morefilterbutton = By.cssSelector("span[class='mr-05 btn-text']");
+    public By ItemTypeFilterSearch = By.cssSelector("div[class='Fl_RiskPartFilter z2parametric-contain'] div:nth-child(1) div:nth-child(2) div:nth-child(1) input:nth-child(1)");
+    public By FirstPagination = By.cssSelector("li[class='pagination-first page-item disabled']");
+    public By PreviousPagination = By.cssSelector("li[class='pagination-prev page-item disabled']");
+    public By NextPagination = By.linkText("Next");
+    public By LastPagination = By.linkText("Last");
+
+
+
 }
